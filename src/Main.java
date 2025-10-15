51e07fdea4289d5c342bdfa4db0dfab5347e1a77
Wilder Andres Duarte Neira <waduarten@ufpso.edu.co>
2025-10-15 10:56:45 -0500

    Respuesta a desafío #1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import principles.interfaces.ForPerso;
 import resources.Empleado;
 
 import java.math.BigDecimal;
@@ -31,7 +32,7 @@ public class Main {
         //To-do: Mostrar empleados por un consumer: Contratados en determinado mes
         Consumer<List<Empleado>> empEnero = emps -> {
             emps.forEach(empleado -> {
-                if(empleado.getFechaIng().getMonth() == Month.JANUARY){
+                if (empleado.getFechaIng().getMonth() == Month.JANUARY) {
                     System.out.println(empleado);
                 }
             });
@@ -43,7 +44,7 @@ public class Main {
         System.out.println("Predicate resultado");
         List<Empleado> infoEmp = new ArrayList<>();
         empleados.forEach(empleado -> {
-            if(deptInfo.test(empleado)){
+            if (deptInfo.test(empleado)) {
                 infoEmp.add(empleado);
             }
         });
@@ -64,32 +65,103 @@ public class Main {
         System.out.println("Consumer resultado");
         empEnero.accept(empleados);
 
+        //To-do: Solucion taller 1: interfaces funcionales
+        System.out.println("Interfaz funcional personalizada");
 
+        System.out.println();
+        System.out.println("REPORTE NOMINA");
+        ForPerso reporteNomina = empleadoP -> "Empleado: " +
+                empleadoP.getNombre() + " " + empleadoP.getApellido() +
+                "\nDepartamento: " + empleadoP.getDepartamento() + "\nCargo: " +
+                empleadoP.getCargo() + "\nSalario: " + empleadoP.getSalario() +
+                "\nFecha ingreso: " + empleadoP.getFechaIng() +
+                "\nEstado: " + empleadoP.getActive();
+        System.out.println(reporteNomina.formato(empleados.getFirst()));
+
+        System.out.println();
+        System.out.println("REPORTE FICHA PERSONAL");
+        ForPerso reporteFichaPersonal = empleadoP -> "Empleado: " +
+                empleadoP.getNombre() + " " + empleadoP.getApellido() + "\nGenero: " +
+                empleadoP.getGenero();
+        System.out.println(reporteFichaPersonal.formato(empleados.getFirst()));
+
+        //To-do: Solucion taller 1: interfaces funcionales
+
+        /* 1. Interface Predicate: filtrar empleados activos con salario menor a 700
+         */
+        System.out.println();
+        System.out.println("Interface predicate resultado");
+        Predicate<Empleado> salarioEmpleado = empleado -> empleado.getActive() &&
+                empleado.getSalario().compareTo(new BigDecimal(700)) == -1;
+
+        List<Empleado> salarioEmp = new ArrayList<>();
+        empleados.forEach(empleado -> {
+            if (salarioEmpleado.test(empleado)) {
+                salarioEmp.add(empleado);
+            }
+        });
+        System.out.println(salarioEmp);
+
+        /* 2. Interface Function: generar un mapa que almacene como clave un departamento y como
+        valor el supervisor de dicho departamento
+        */
+        System.out.println("Interface function resultado");
+        Function<List<Empleado>, Map<String, String>> supervisorDepartamento = empleadoList -> {
+            Map<String, String> supervisor = new HashMap<>();
+            empleadoList.forEach(empleado -> {
+                if (empleado.getCargo().startsWith("Supervisor")) {
+                    supervisor.put(empleado.getDepartamento(), empleado.getNombre() + " " + empleado.getApellido());
+                }
+            });
+            return supervisor;
+        };
+        Map<String, String> supervisorsDep = supervisorDepartamento.apply(empleados);
+        System.out.println(supervisorsDep);
+
+        /* 3. Interface Consumer: imprimir en consola un listado de empleados de un determinado departamento
+         */
+        System.out.println("Interface consumer resultado");
+        Consumer<List<Empleado>> listadoEmpleados = listaEmpleados -> {
+            listaEmpleados.forEach(empleado -> {
+                if (empleado.getDepartamento().equals("Informática")) {
+                    System.out.println(empleado);
+                }
+            });
+        };
+        listadoEmpleados.accept(empleados);
+
+        /* 4. Interface Comparator: ordena a los empleados por su apellido en orden alfabetico
+         */
+        System.out.println("Interface comparator resultado");
+        Comparator<Empleado> ordenarPorApellido = Comparator.comparing(Empleado::getApellido);
+
+        List<Empleado> empleadosPorApellido = new ArrayList<>(empleados);
+        empleadosPorApellido.sort(ordenarPorApellido);
+        System.out.println(empleadosPorApellido);
     }
 
-    public static void loadEmpleados(List<Empleado> empleadoList){
+    public static void loadEmpleados(List<Empleado> empleadoList) {
         empleadoList.add(new Empleado("María", "Rodríguez", "F", "Contabilidad", "Asistente Contable", new BigDecimal(700), LocalDate.parse("2021-04-01")));
-        empleadoList.add(new Empleado("Juan", "Gutierrez", "M", "Talento Humano", "Reclutador", new BigDecimal(500), LocalDate.parse("2023-03-11"), LocalDate.parse("2024-04-01"), false));
-        empleadoList.add(new Empleado("José", "Albornoz", "M","Contabilidad", "Asistente Contable", new BigDecimal(800), LocalDate.parse("2020-08-15"), LocalDate.parse("2023-05-01"), false));
+        empleadoList.add(new Empleado("Juan", "Gutierrez", "M", "Talento Humano", "Reclutador", new BigDecimal(500), LocalDate.parse("2023-03-11"), LocalDate.parse("2024-04-01"), true));
+        empleadoList.add(new Empleado("José", "Albornoz", "M", "Contabilidad", "Asistente Contable", new BigDecimal(800), LocalDate.parse("2020-08-15"), LocalDate.parse("2023-05-01"), true));
         empleadoList.add(new Empleado("Julián", "Flores", "M", "Informática", "Soporte TI", new BigDecimal(800), LocalDate.parse("2023-11-01")));
-        empleadoList.add(new Empleado("Camila", "Mendoza","F", "Informática", "Desarrollador UI/UX", new BigDecimal(1000), LocalDate.parse("2021-07-08")));
+        empleadoList.add(new Empleado("Camila", "Mendoza", "F", "Informática", "Desarrollador UI/UX", new BigDecimal(1000), LocalDate.parse("2021-07-08")));
         empleadoList.add(new Empleado("Camilo", "López", "M", "Contabilidad", "Supervisor Contable", new BigDecimal(1500), LocalDate.parse("2020-04-11")));
         empleadoList.add(new Empleado("Manuel", "Játiva", "M", "Contabilidad", "Asistente Contable", new BigDecimal(850), LocalDate.parse("2023-06-03")));
         empleadoList.add(new Empleado("Carlos", "Franco", "M", "Talento Humano", "Reclutador", new BigDecimal(650), LocalDate.parse("2023-01-07"), LocalDate.parse("2024-12-09"), false));
         empleadoList.add(new Empleado("Raúl", "Echeverría", "M", "Informática", "Infraestructura TI", new BigDecimal(950), LocalDate.parse("2020-02-14")));
         empleadoList.add(new Empleado("Estefanía", "Mendoza", "F", "Talento Humano", "Supervisora TH", new BigDecimal(1600), LocalDate.parse("2021-09-21")));
         empleadoList.add(new Empleado("Julie", "Flores", "F", "Informática", "Desarrollador", new BigDecimal(1200), LocalDate.parse("2021-12-10")));
-        empleadoList.add(new Empleado("Melissa", "Morocho", "F","Contabilidad", "Asistente Contable", new BigDecimal(820), LocalDate.parse("2022-05-22"), LocalDate.parse("2023-07-09"), false));
+        empleadoList.add(new Empleado("Melissa", "Morocho", "F", "Contabilidad", "Asistente Contable", new BigDecimal(820), LocalDate.parse("2022-05-22"), LocalDate.parse("2023-07-09"), false));
         empleadoList.add(new Empleado("Camila", "Mendez", "F", "Contabilidad", "Asistente Cuentas", new BigDecimal(860), LocalDate.parse("2020-10-01")));
-        empleadoList.add(new Empleado("José", "Rodríguez", "M","Informática", "Tester QA", new BigDecimal(1100), LocalDate.parse("2021-10-01")));
-        empleadoList.add(new Empleado("Esteban", "Gutierrez","M", "Talento Humano", "Reclutador", new BigDecimal(700), LocalDate.parse("2023-04-01")));
-        empleadoList.add(new Empleado("María", "López","F", "Contabilidad", "Asistente Contable", new BigDecimal(840), LocalDate.parse("2020-02-20"), LocalDate.parse("2024-07-15"), false));
-        empleadoList.add(new Empleado("Cecilia", "Marín","F", "Informática", "Supervisora TI", new BigDecimal(2000), LocalDate.parse("2020-04-21")));
-        empleadoList.add(new Empleado("Edison", "Cáceres","M", "Informática", "Desarrollador TI", new BigDecimal(1300), LocalDate.parse("2023-07-07")));
-        empleadoList.add(new Empleado("María", "Silva", "F","Contabilidad", "Asistente Contable", new BigDecimal(900), LocalDate.parse("2021-11-15"), LocalDate.parse("2022-08-09"), false));
+        empleadoList.add(new Empleado("José", "Rodríguez", "M", "Informática", "Tester QA", new BigDecimal(1100), LocalDate.parse("2021-10-01")));
+        empleadoList.add(new Empleado("Esteban", "Gutierrez", "M", "Talento Humano", "Reclutador", new BigDecimal(700), LocalDate.parse("2023-04-01")));
+        empleadoList.add(new Empleado("María", "López", "F", "Contabilidad", "Asistente Contable", new BigDecimal(840), LocalDate.parse("2020-02-20"), LocalDate.parse("2024-07-15"), true));
+        empleadoList.add(new Empleado("Cecilia", "Marín", "F", "Informática", "Supervisora TI", new BigDecimal(2000), LocalDate.parse("2020-04-21")));
+        empleadoList.add(new Empleado("Edison", "Cáceres", "M", "Informática", "Desarrollador TI", new BigDecimal(1300), LocalDate.parse("2023-07-07")));
+        empleadoList.add(new Empleado("María", "Silva", "F", "Contabilidad", "Asistente Contable", new BigDecimal(900), LocalDate.parse("2021-11-15"), LocalDate.parse("2022-08-09"), true));
 
     }
-
 
 
 }
---
+++ src/principles/interfaces/ForPerso.java
@@ -0,0 +1,6 @@
+package principles.interfaces;
+
+import resources.Empleado;
+public class ForPerso {
+    String formato(Empleado empleado);
+}
